342c247b5faa7971ecda139b663c5fca01602c79
Eros Daniel <erosd99@gmail.com>
2019-04-28 15:02:22 +0200

    Added news page rendering to UI class

--- src/js/Controller/main.js
+++ src/js/Controller/main.js
@@ -1,7 +1,20 @@
 const ui = new UI();
 const menuButtons = document.querySelectorAll("nav li");
-
+const articles = [
+  {
+    headline: "First Article",
+    picture: "Balaton-móló.png",
+    content: "asdasdasdasdasdasdasdasdqweqwdrasfasdfaw"
+  },
+  {
+    headline: "Second Article",
+    picture: "Balaton-part.png",
+    content: "asdqweinqibncvodjoaisdpiqwepkq;lfnzl;c;ak"
+  }
+];
 //Event Handlers
 menuButtons.forEach(button =>
-  button.addEventListener("click", renderActivePage)
+  button.addEventListener("click", () =>
+    ui.renderPage(menuButtons, button.className, articles)
+  )
 );
--- src/js/View/ui.js
+++ src/js/View/ui.js
@@ -1,5 +1,58 @@
-class UI{
-    constructor() {
-        this.display = document.querySelector('main');
+class UI {
+  constructor() {
+    this.display = document.querySelector("main");
+  }
+
+  renderPage(parent, pageName, src) {
+    this.turnNavLinkToActive(parent, pageName);
+    this.renderBackgroundFor(pageName, src);
+  }
+
+  turnNavLinkToActive(parent, pageName) {
+    parent.forEach(link => {
+      if (link.className === pageName) {
+        link.classList.add("active");
+      } else if (link.classList.contains("active")) {
+        link.classList.remove("active");
+      }
+    });
+  }
+
+  renderBackgroundFor(pageName, articles) {
+    switch (pageName) {
+      case "nav-news":
+        this.renderNews(articles);
+        break;
+      case "nav-gallery":
+        this.renderGallery();
+        break;
+      case "nav-discography":
+        this.renderDiscography();
+        break;
+      case "nav-td":
+        this.renderTourDates();
+        break;
+      case "nav-store":
+        this.renderStore();
+        break;
+      case "nav-connect":
+        this.renderConnect();
+        break;
     }
+  }
+
+  renderNews(articles) {
+    this.display.innerHTML = "";
+    articles.forEach(article => {
+      const container = document.createElement("div");
+      const html = `<h1>${article.headline}</h1>
+      <img src='src/Pictures/${article.picture}'>
+      <p>${article.content}</p>`;
+      container.className = "container";
+      container.innerHTML = html;
+      this.display.appendChild(container);
+    });
+  }
+
+  renderGallery()
 }
